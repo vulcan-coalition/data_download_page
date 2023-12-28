08ec7a3665e7c1c97fe809a328fb30747917d57b
velodica <ravipas.apk@gmail.com>
2023-12-28 16:27:05 +0700

    adjust

--- main.js
+++ main.js
@@ -20,9 +20,7 @@
     $(window).scroll(function () {
         if ($(this).scrollTop() > 45) {
             $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
+        } 
     });
 
 
