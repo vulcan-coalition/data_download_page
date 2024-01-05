1fb8aa1ce5fc8d609d5806b211fc09b373a59e69
Ananya Kuasakunrungroj <39475170+izatea555@users.noreply.github.com>
2024-01-05 16:47:29 +0700

    add download progress bar

--- main.js
+++ main.js
@@ -83,6 +83,23 @@
             }
         }
     });
+
+    //dowload bar
+    function startDownload() {
+        var elem = document.getElementById("progressBar"); 
+        var width = 1;
+        var id = setInterval(frame, 100);
+    
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+            } else {
+                width++; 
+                elem.style.width = width + '%'; 
+            }
+        }
+    }
+    
     
     
 
